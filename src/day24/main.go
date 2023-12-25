07fe3d3c85100a1c5bbda166bb0d72326b5a86e0
omotto <oscar.motto@mail.schwarz>
2023-12-25 22:39:59 +0100

    day 24 finished both parts

--- src/day24/main.go
+++ src/day24/main.go
@@ -96,8 +96,197 @@ func getCrossedHailstones(in []string, min, max float64) (int, error) {
 	}
 }
 
+func intersectXY(h1, h2 hailstone, vx, vy float64) ([2]float64, error) {
+	x1 := h1.x
+	y1 := h1.y
+	x2 := h1.x + h1.dx - vx
+	y2 := h1.y + h1.dy - vy
+	x3 := h2.x
+	y3 := h2.y
+	x4 := h2.x + h2.dx - vx
+	y4 := h2.y + h2.dy - vy
+	if denominator := (y4-y3)*(x2-x1) - (x4-x3)*(y2-y1); denominator == 0 {
+		return [2]float64{}, errors.New("invalid denominator")
+	} else {
+		ua := ((x4-x3)*(y1-y3) - (y4-y3)*(x1-x3)) / denominator
+		return [2]float64{x1 + ua*(x2-x1), y1 + ua*(y2-y1)}, nil
+	}
+}
+
+func intersectYZ(h1, h2 hailstone, vy, vz float64) ([2]float64, error) {
+	y1 := h1.y
+	z1 := h1.z
+	y2 := h1.y + h1.dy - vy
+	z2 := h1.z + h1.dz - vz
+	y3 := h2.y
+	z3 := h2.z
+	y4 := h2.y + h2.dy - vy
+	z4 := h2.z + h2.dz - vz
+	if denominator := (z4-z3)*(y2-y1) - (y4-y3)*(z2-z1); denominator == 0 {
+		return [2]float64{}, errors.New("invalid denominator")
+	} else {
+		ua := ((y4-y3)*(z1-z3) - (z4-z3)*(y1-y3)) / denominator
+		return [2]float64{y1 + ua*(y2-y1), z1 + ua*(z2-z1)}, nil
+	}
+}
+
+func intersectXZ(h1, h2 hailstone, vx, vz float64) ([2]float64, error) {
+	x1 := h1.x
+	z1 := h1.z
+	x2 := h1.x + h1.dx - vx
+	z2 := h1.z + h1.dz - vz
+	x3 := h2.x
+	z3 := h2.z
+	x4 := h2.x + h2.dx - vx
+	z4 := h2.z + h2.dz - vz
+	if denominator := (z4-z3)*(x2-x1) - (x4-x3)*(z2-z1); denominator == 0 {
+		return [2]float64{}, errors.New("invalid denominator")
+	} else {
+		ua := ((x4-x3)*(z1-z3) - (z4-z3)*(x1-x3)) / denominator
+		return [2]float64{x1 + ua*(x2-x1), z1 + ua*(z2-z1)}, nil
+	}
+}
+
+func findSourceVelocity(in []string, threshold int, rangeX, rangeY, rangeZ int) ([]hailstone, error) {
+	var h []hailstone
+	if hailstones, err := parseEquations(in); err != nil {
+		return h, err
+	} else {
+		for vz := -rangeZ; vz <= rangeZ; vz++ {
+			for vx := -rangeX; vx <= rangeX; vx++ {
+				for vy := -rangeY; vy <= rangeY; vy++ {
+					skip := false
+					count := 0
+					for i := 0; i < len(hailstones); i++ {
+						for j := i + 1; j < len(hailstones); j++ {
+							intXY, eXY := intersectXY(hailstones[i], hailstones[j], float64(vx), float64(vy))
+							intYZ, eYZ := intersectYZ(hailstones[i], hailstones[j], float64(vy), float64(vz))
+							intXZ, eXZ := intersectXZ(hailstones[i], hailstones[j], float64(vx), float64(vz))
+							// Parallel count {
+							parallelCount := 0
+							if eXY != nil {
+								parallelCount++
+							}
+							if eYZ != nil {
+								parallelCount++
+							}
+							if eXZ != nil {
+								parallelCount++
+							}
+							if parallelCount > 2 {
+								skip = true
+								break
+							}
+							matches := 0
+							var tI, tJ float64
+							if eXY == nil {
+								var timeI float64
+								if hailstones[i].dx-float64(vx) != 0 {
+									timeI = (intXY[0] - hailstones[i].x) / (hailstones[i].dx - float64(vx))
+								} else {
+									timeI = (intXY[1] - hailstones[i].y) / (hailstones[i].dy - float64(vy))
+								}
+								var timeJ float64
+								if hailstones[j].dx-float64(vx) != 0 {
+									timeJ = (intXY[0] - hailstones[j].x) / (hailstones[j].dx - float64(vx))
+								} else {
+									timeJ = (intXY[1] - hailstones[j].y) / (hailstones[j].dy - float64(vy))
+								}
+								if tI == 0 {
+									tI = timeI
+								}
+								if tJ == 0 {
+									tJ = timeJ
+								}
+								if timeI > 0 && timeJ > 0 && tI == timeI && tJ == timeJ {
+									matches++
+								}
+							}
+							if eYZ == nil {
+								var timeI float64
+								if hailstones[i].dy-float64(vy) != 0 {
+									timeI = (intYZ[0] - hailstones[i].y) / (hailstones[i].dy - float64(vy))
+								} else {
+									timeI = (intYZ[1] - hailstones[i].z) / (hailstones[i].dz - float64(vz))
+								}
+								var timeJ float64
+								if hailstones[j].dy-float64(vy) != 0 {
+									timeJ = (intYZ[0] - hailstones[j].y) / (hailstones[j].dy - float64(vy))
+								} else {
+									timeJ = (intYZ[1] - hailstones[j].z) / (hailstones[j].dz - float64(vz))
+								}
+								if tI == 0 {
+									tI = timeI
+								}
+								if tJ == 0 {
+									tJ = timeJ
+								}
+								if timeI > 0 && timeJ > 0 && tI == timeI && tJ == timeJ {
+									matches++
+								}
+							}
+
+							if eXZ == nil {
+								var timeI float64
+								if hailstones[i].dx-float64(vx) != 0 {
+									timeI = (intXZ[0] - hailstones[i].x) / (hailstones[i].dx - float64(vx))
+								} else {
+									timeI = (intXZ[1] - hailstones[i].z) / (hailstones[i].dz - float64(vz))
+								}
+								var timeJ float64
+								if hailstones[j].dx-float64(vx) != 0 {
+									timeJ = (intXZ[0] - hailstones[j].x) / (hailstones[j].dx - float64(vx))
+								} else {
+									timeJ = (intXZ[1] - hailstones[j].z) / (hailstones[j].dz - float64(vz))
+								}
+								if tI == 0 {
+									tI = timeI
+								}
+								if tJ == 0 {
+									tJ = timeJ
+								}
+								if timeI > 0 && timeJ > 0 && tI == timeI && tJ == timeJ {
+									matches++
+								}
+							}
+							if matches >= 2 {
+								count++
+							} else {
+								skip = true
+								break
+							}
+							if count >= threshold && parallelCount == 0 {
+								exist := false
+								for _, v := range h {
+									if v.x == intXY[0] && v.y == intXY[1] && v.z == intYZ[1] {
+										exist = true
+										break
+									}
+								}
+								if !exist {
+									h = append(h, hailstone{
+										x: intXY[0],
+										y: intXY[1],
+										z: intYZ[1],
+									})
+								}
+							}
+						}
+						if skip {
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+	return h, nil
+}
+
 func main() {
 	abs, _ := filepath.Abs("src/day24/input.txt")
 	output, _ := file.ReadInput(abs)
 	fmt.Println(getCrossedHailstones(output, 200000000000000.0, 400000000000000.0))
+	h, _ := findSourceVelocity(output, 20, 500, 500, 500)
+	fmt.Println(h[0].x + h[0].y + h[0].z) // 856642398547748
 }
--- src/day24/main_test.go
+++ src/day24/main_test.go
@@ -36,3 +36,33 @@ func TestGetCrossedHailstones(t *testing.T) {
 		})
 	}
 }
+
+func TestFindSourceVelocity(t *testing.T) {
+	tcs := []struct {
+		desc      string
+		input     []string
+		threshold int
+		expected  []hailstone
+	}{
+		{
+			desc: "expected 2",
+			input: []string{
+				"19, 13, 30 @ -2,  1, -2",
+				"18, 19, 22 @ -1, -1, -2",
+				"20, 25, 34 @ -2, -2, -4",
+				"12, 31, 28 @ -1, -2, -1",
+				"20, 19, 15 @  1, -5, -3",
+			},
+			threshold: 5,
+			expected:  []hailstone{{x: 24, y: 13, z: 10}},
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			v, _ := findSourceVelocity(tc.input, tc.threshold, 10, 10, 10)
+			if diff := cmp.Diff(tc.expected, v, cmp.AllowUnexported(hailstone{})); diff != "" {
+				t.Errorf("value has diff %s", diff)
+			}
+		})
+	}
+}
